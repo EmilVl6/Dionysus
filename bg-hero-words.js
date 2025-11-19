9baa594589c3512d0837f1a9b5d43d7fb3dcae60
emilv <93434155+EmilVl6@users.noreply.github.com>
2025-11-19 17:46:16 +1100

    Personal Site

---
+++ bg-hero-words.js
@@ -0,0 +1,15 @@
+const words = [
+  'quite a great lover',
+  'athleisurewear user',
+  'retired pastry chef',
+  'woodsman on mondays',
+  'sending out an SOS.',
+  'super hans advocate',
+  'MONORAIL! MONORAIL!',
+  'former living child',
+  'next pope contender',
+  'composing cacophony',
+  'karaoke connoisseur'
+];
+
+window.bgHeroWords = words;
