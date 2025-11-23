596b91c14ae86906ede9d4345dda4a3ef06d618a
emilv <93434155+EmilVl6@users.noreply.github.com>
2025-11-23 17:46:27 +1100

    Personal Site

--- bg-hero-words.js
+++ bg-hero-words.js
@@ -1,20 +1,16 @@
 // Emil Vinod Descriptors
 const words = [
-  'quite a great lover',
   'athleisurewear user',
-  'names head knobhead',
   'next pope contender',
   'retired pastry chef',
   'former living child',    
   'woodsman on mondays',
-  'sending out an SOS.',
   'super hans advocate',
   'MONORAIL! MONORAIL!',
   'composing cacophony',
   'karaoke connoisseur',
   'amateur tooth fairy',
-  'cant ballroom dance',
-  'associates delusion'
+  'dance troupe leader'
 ];
 
 window.bgHeroWords = words;
